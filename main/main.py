f2e42ed83eb657e0a55e2b750f9edfcd372ec589
Sreram K <sreramk1@gmail.com>
2020-01-15 03:03:52 +0530

    Add files via upload

--- main/main.py
+++ main/main.py
@@ -1,21 +1,40 @@
-from byn_auto.binary_automata import Graph
+from byn_auto.graph_constructor import Graph
 import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
     gc = Graph()
-    gc.add_sequence([1, 0, 1, 1, 0, 1, 1])
-    gc.add_sequence([1, 0, 1, 1, 0, 1, 0])
-    gc.add_sequence([0, 0, 1, 1, 0, 1, 0])
-    gc.add_sequence([0, 0, 1, 1, 0, 1, 1])
-    gc.add_sequence([0, 0, 1, 1, 0, 1, 1])
-    gc.add_sequence([1, 0, 0, 0, 0, 1, 1])
+
+    gc.add_sequence([0,0,0])
+    gc.add_sequence([0,0,1])
+    gc.add_sequence([0,1,0])
+    gc.add_sequence([0,1,1])
+
+    # gc.add_sequence([1, 0, 0])
+    # gc.add_sequence([1, 0, 1])
+    # gc.add_sequence([1, 1, 0])
+    # gc.add_sequence([1, 1, 1])
+    #
+    # gc.add_sequence([1, 0, 1, 1, 0, 1, 1])
+    # gc.add_sequence([1, 0, 1, 1, 0, 1, 0])
+    # gc.add_sequence([0, 0, 1, 1, 0, 1, 0])
+    # gc.add_sequence([0, 0, 1, 1, 0, 1, 1])
+    # gc.add_sequence([0, 0, 1, 1, 0, 1, 1])
+    # gc.add_sequence([1, 0, 0, 0, 0, 1, 1])
+    #
+    # gc.add_sequence([1, 0, 0, 0, 0, 1])
+    # gc.add_sequence([1, 0, 0, 0, 0])
+    # gc.add_sequence([1, 0, 0, 0])
+    # gc.add_sequence([1, 0, 0])
+    #
+    # gc.add_sequence([2,3,3,2,1])
+    # gc.add_sequence([2,3,2,1,1])
+
 
     print(" Before simplification: \n")
     gc.display_graph()
     gc.plot_graph()
 
-    gc.simplify_graph()
     gc.simplify_graph()
 
     print(" \n After Simplification: \n ")
